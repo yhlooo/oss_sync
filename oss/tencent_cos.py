5ec9be332e106592ec456d14a8e4b17de42ead99
keyboardluo <keyboardluo@tencent.com>
2020-12-06 19:12:56 +0800

    refactor: 重构腾讯云 COS 相关模块

--- oss/tencent_cos.py
+++ oss/tencent_cos.py
@@ -8,33 +8,44 @@
 import logging
 from typing import Dict, List, Optional, Tuple
 
-from qcloud_cos import CosConfig
-from qcloud_cos import CosS3Client
+from qcloud_cos import CosConfig, CosS3Client
+from qcloud_cos.cos_exception import CosClientError, CosServiceError
 
 from .abstract_oss import OssBucket
 
 
-logger = logging.getLogger(__name__)
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 class QcloudCosBucket(OssBucket):
     def __init__(self, config: Dict[str, str]):
-        self.bucket = config.get('bucket')
-        oss_config = CosConfig(
+        """初始化
+
+        Args:
+            config: 腾讯云 COS 初始化配置
+
+        """
+
+        self.bucket: str = config.get('bucket')
+
+        oss_config: CosConfig = CosConfig(
             Region=config.get('region'),
             SecretId=config.get('secret_id'),
             SecretKey=config.get('secret_key'),
             Token=config.get('token'),
             Scheme=config.get('scheme')
         )
-        self.client = CosS3Client(oss_config)
 
-    def list_objects(self) -> List[Tuple[str, str]]:
+        self.client: CosS3Client = CosS3Client(oss_config)
+
+    def list_objects(self) -> Optional[List[Tuple[str, str]]]:
         """列出对象
 
         列出 Bucket 中的对象
 
         Returns:
+            正常的话返回以下格式内容
+
             [
                 (obj_key_1, obj_md5_1),
                 (obj_key_2, obj_md5_2),
@@ -42,18 +53,38 @@ class QcloudCosBucket(OssBucket):
                 # ...
             ]
 
+            查询失败的话返回 None
+
         """
 
-        res = []
+        # 结果对象列表
+        objs_list = []
+
+        # 开始的 Key （用于标记分页）
         marker = '/'
+
         while marker:
-            response = self.client.list_objects(Bucket=self.bucket, Marker=marker)
-            res.extend([
+
+            logger.debug(f'Marker = \'{marker}\'')
+
+            try:
+                ret = self.client.list_objects(Bucket=self.bucket, Marker=marker)
+                logger.debug(f'ret = {ret}')
+
+            except (CosClientError, CosServiceError) as err:
+                logger.error(f'请求腾讯云 COS 失败： {type(err).__name__}: {err}')
+                return None
+
+            objs_list.extend([
                 (obj.get('Key'), obj.get('ETag')[1:-1])
-                for obj in response.get('Contents', [])
+                for obj
+                in ret.get('Contents', [])
             ])
-            marker = response.get('NextMarker')
-        return res
+            marker = ret.get('NextMarker')
+
+            logger.debug(f'NextMarker = \'{marker}\'')
+
+        return objs_list
 
     def put_object(self, obj_key: str, data: bytes) -> bool:
         """上传对象
@@ -70,10 +101,18 @@ class QcloudCosBucket(OssBucket):
         """
 
         try:
-            self.client.put_object(self.bucket, data, obj_key, EnableMD5=True)
-        except Exception as e:
-            logger.error(e)
+            ret = self.client.put_object(
+                Bucket=self.bucket,
+                Key=obj_key,
+                Body=data,
+                EnableMD5=True
+            )
+            logger.debug(f'ret = {ret}')
+
+        except (CosClientError, CosServiceError) as err:
+            logger.error(f'{type(err).__name__}: {err}')
             return False
+
         return True
 
     def get_object(self, obj_key: str) -> Optional[bytes]:
@@ -90,12 +129,16 @@ class QcloudCosBucket(OssBucket):
         """
 
         try:
-            response = self.client.get_object(self.bucket, obj_key)
-            file = response['Body'].get_raw_stream().read()
-        except Exception as e:
-            logger.error(e)
+            ret = self.client.get_object(Bucket=self.bucket, Key=obj_key)
+            logger.debug(f'ret = {ret}')
+
+            file_content = ret['Body'].get_raw_stream().read()
+
+        except (CosClientError, CosServiceError) as err:
+            logger.error(f'{type(err).__name__}: {err}')
             return None
-        return file
+
+        return file_content
 
     def del_object(self, obj_key: str) -> bool:
         """删除对象
@@ -111,8 +154,11 @@ class QcloudCosBucket(OssBucket):
         """
 
         try:
-            self.client.delete_object(self.bucket, obj_key)
-        except Exception as e:
-            logger.error(e)
+            ret = self.client.delete_object(Bucket=self.bucket, Key=obj_key)
+            logger.debug(f'ret = {ret}')
+
+        except (CosClientError, CosServiceError) as err:
+            logger.error(f'{type(err).__name__}: {err}')
             return False
+
         return True
